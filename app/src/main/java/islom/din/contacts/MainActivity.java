4dfe42d2583056e463c14ea4cb32cd66cdd41c7f
Islom Nuridinov <islom.din.nur@gmail.com>
2020-11-25 23:38:14 +0500

    made first screen

--- app/src/main/java/islom/din/contacts/MainActivity.java
+++ app/src/main/java/islom/din/contacts/MainActivity.java
@@ -2,13 +2,30 @@ package islom.din.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
